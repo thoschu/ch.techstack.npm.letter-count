763b5cba9d22a7033d7d7670822796ae7934835d
Tom <thoschulte@gmail.com>
2018-01-07 22:41:37 +0100

    refactor

---
+++ main.js
@@ -0,0 +1,25 @@
+// const LC = require('./lib/app');
+//
+// console.log(LC.count(' Tom \n S. 11  '));
+
+process.stdin.on('readable', () => {
+    let data = process.stdin.read();
+
+    console.log(typeof data);
+
+    if (data !== null) {
+        process.stdout.write(`Data: ${data}`);
+    }
+});
+
+// var input = "Thomas Schulte";
+//
+// var chars = input.length;
+// var words = input.split(/[\t \n]/).filter(e => e !== '').length;
+// var lines = input.split('\n').length;
+//
+// console.log(chars);
+// console.log(words);
+// console.log(lines);
+//
+console.log(process.argv.pop());
