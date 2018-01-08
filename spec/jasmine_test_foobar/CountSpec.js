ebbc1a51517eeabe4fa9921d0343c3004b96381c
Tom <thoschulte@gmail.com>
2018-01-09 00:18:34 +0100

    test

--- spec/jasmine_test_foobar/CountSpec.js
+++ spec/jasmine_test_foobar/CountSpec.js
@@ -10,49 +10,8 @@ describe("CountTest", function () {
 
     it("should be able to count the string", function () {
         var result = LC.count("thomasschulte").letters;
-        var answer = 7;
+        var answer = 13;
 
         expect(result).toEqual(answer);
     });
-
-    // describe("when xxx has been paused", function () {
-    //     beforeEach(function () {
-    //         // player.play(song);
-    //         // player.pause();
-    //     });
-    //
-    //     it("should indicate that the xxx is currently paused", function () {
-    //         // expect(player.isPlaying).toBeFalsy();
-    //         //
-    //         // // demonstrates use of 'not' with a custom matcher
-    //         // expect(player).not.toBePlaying(song);
-    //     });
-    //
-    //     it("should be possible to resume", function () {
-    //         // player.resume();
-    //         // expect(player.isPlaying).toBeTruthy();
-    //         // expect(player.currentlyPlayingSong).toEqual(song);
-    //     });
-    // });
-    //
-    // // demonstrates use of spies to intercept and test method calls
-    // it("tells the current xxx if the user has made it a favorite", function () {
-    //     // spyOn(song, 'persistFavoriteStatus');
-    //     //
-    //     // player.play(song);
-    //     // player.makeFavorite();
-    //     //
-    //     // expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-    // });
-    //
-    // //demonstrates use of expected exceptions
-    // describe("#resume", function () {
-    //     it("should throw an exception if ...", function () {
-    //         // player.play(song);
-    //         //
-    //         // expect(function() {
-    //         //   player.resume();
-    //         // }).toThrowError("song is already playing");
-    //     });
-    // });
 });
