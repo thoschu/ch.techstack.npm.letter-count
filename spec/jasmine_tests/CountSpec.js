9bf178a564e6df28d97e26a8e5dd07cb3c835a4a
Tom <thoschulte@gmail.com>
2018-01-12 17:52:08 +0100

    refactor tests

--- spec/jasmine_tests/CountSpec.js
+++ spec/jasmine_tests/CountSpec.js
@@ -11,135 +11,145 @@ describe("letter-count.js tests", function () {
         });
 
         it("1. should be able to count the number of letters in the given string", function () {
-            let result =  R.prop('letters', counted),
-                answer = 20;
-            expect(result).toEqual(answer);
+            let result =  R.prop('letters', counted);
+
+            expect(20).toEqual(result);
         });
 
         it("2. should be able to count the number of lines in the given string", function () {
-            let result =  R.prop('lines', counted),
-                answer = 2;
-            expect(result).toEqual(answer);
+            let result =  R.prop('lines', counted);
+
+            expect(2).toEqual(result);
         });
 
         it("3. should be able to count the number of characters in the given string", function () {
-            let result =  R.prop('chars', counted),
-                answer = 33;
-            expect(result).toEqual(answer);
+            let result =  R.prop('chars', counted);
+
+            expect(33).toEqual(result);
         });
 
         it("4. should be able to count the number of words in the given string", function () {
-            let result =  R.prop('words', counted),
-                answer = 3;
-            expect(result).toEqual(answer);
+            let result =  R.prop('words', counted);
+
+            expect(3).toEqual(result);
         });
 
         it("5. should be able to count the number of numbers in the given string", function () {
-            let result =  R.prop('numbers', counted),
-                answer = 1;
-            expect(result).toEqual(answer);
+            let result =  R.prop('numbers', counted);
+
+            expect(1).toEqual(result);
         });
 
         it("6. should be able to count the number of wordsigns in the given string", function () {
-            let result =  R.prop('wordsigns', counted),
-                answer = 4;
-            expect(result).toEqual(answer);
+            let result =  R.prop('wordsigns', counted);
+
+            expect(4).toEqual(result);
         });
     });
 
     describe("B. the countFromFile method", function() {
+        let currentWorkingDirectory = process.cwd(),
+            filePath = '/test/input.txt',
+            pathToFile = R.concat(currentWorkingDirectory, filePath);
+
         describe("b.A. the countFromFile method without options", function() {
             beforeAll(function () {
-                let pathToFile = R.concat(process.cwd(), '/test/input.txt');
                 counted = LC.countFromFile(pathToFile);
             });
 
             it("1. should be able to count the number of wordsigns in the given file", function () {
-                let result =  R.prop('wordsigns', counted),
-                    answer = 2;
-                expect(result).toEqual(answer);
+                let result =  R.prop('wordsigns', counted);
+
+                expect(2).toEqual(result);
             });
 
             it("2. should be able to count the number of numbers in the given file", function () {
-                let result =  R.prop('numbers', counted),
-                    answer = 3;
-                expect(result).toEqual(answer);
+                let result =  R.prop('numbers', counted);
+
+                expect(3).toEqual(result);
             });
 
             it("3. should be able to count the number of words in the given file", function () {
-                let result =  R.prop('words', counted),
-                    answer = 2;
-                expect(result).toEqual(answer);
+                let result =  R.prop('words', counted);
+
+                expect(2).toEqual(result);
             });
 
             it("4. should be able to count the number of characters in the given file", function () {
-                let result =  R.prop('chars', counted),
-                    answer = 23;
-                expect(result).toEqual(answer);
+                let result =  R.prop('chars', counted);
+
+                expect(23).toEqual(result);
             });
 
             it("5. should be able to count the number of lines in the given file", function () {
-                let result =  R.prop('lines', counted),
-                    answer = 2;
-                expect(result).toEqual(answer);
+                let result =  R.prop('lines', counted);
+
+                expect(2).toEqual(result);
             });
 
             it("6. should be able to count the number of letters in the given file", function () {
-                let result =  R.prop('letters', counted),
-                    answer = 14;
-                expect(result).toEqual(answer);
+                let result =  R.prop('letters', counted);
+
+                expect(14).toEqual(result);
             });
         });
 
         describe("b.B. the countFromFile method with options", function() {
-            let pathToFile = null;
+            let countFromFile = null,
+                countFromFileFn = (opt) => {
+                    return countFromFile(pathToFile, opt);
+                };
+
+            beforeAll(function () {
+                countFromFile = R.prop('countFromFile', LC);
+            });
 
             it("1. should be able to count the number of wordsigns in the given file", function () {
-                pathToFile = R.concat(process.cwd(), '/test/input.txt');
-                counted = LC.countFromFile(pathToFile, '-ws');
-                let result =  R.prop('wordsigns', counted),
-                    answer = 2;
-                expect(result).toEqual(answer);
+                counted = countFromFileFn('-ws');
+
+                let result =  R.prop('wordsigns', counted);
+
+                expect(2).toEqual(result);
             });
 
             it("2. should be able to count the number of numbers in the given file", function () {
-                pathToFile = R.concat(process.cwd(), '/test/input.txt');
-                counted = LC.countFromFile(pathToFile, '-n');
-                let result =  R.prop('numbers', counted),
-                    answer = 3;
-                expect(result).toEqual(answer);
+                counted = countFromFileFn('-n');
+
+                let result =  R.prop('numbers', counted);
+
+                expect(3).toEqual(result);
             });
 
             it("3. should be able to count the number of words in the given file", function () {
-                pathToFile = R.concat(process.cwd(), '/test/input.txt');
-                counted = LC.countFromFile(pathToFile, '-w');
-                let result =  R.prop('words', counted),
-                    answer = 2;
-                expect(result).toEqual(answer);
+                counted = countFromFileFn('-w');
+
+                let result =  R.prop('words', counted);
+
+                expect(2).toEqual(result);
             });
 
             it("4. should be able to count the number of characters in the given file", function () {
-                pathToFile = R.concat(process.cwd(), '/test/input.txt');
-                counted = LC.countFromFile(pathToFile, '-c');
-                let result =  R.prop('chars', counted),
-                    answer = 23;
-                expect(result).toEqual(answer);
+                counted = countFromFileFn('-c');
+
+                let result =  R.prop('chars', counted);
+
+                expect(23).toEqual(result);
             });
 
             it("5. should be able to count the number of lines in the given file", function () {
-                pathToFile = R.concat(process.cwd(), '/test/input.txt');
-                counted = LC.countFromFile(pathToFile, '-ln');
-                let result =  R.prop('lines', counted),
-                    answer = 2;
-                expect(result).toEqual(answer);
+                counted = countFromFileFn('-ln');
+
+                let result =  R.prop('lines', counted);
+
+                expect(2).toEqual(result);
             });
 
             it("6. should be able to count the number of letters in the given file", function () {
-                pathToFile = R.concat(process.cwd(), '/test/input.txt');
-                counted = LC.countFromFile(pathToFile, '-l');
-                let result =  R.prop('letters', counted),
-                    answer = 14;
-                expect(result).toEqual(answer);
+                counted = countFromFileFn('-l');
+
+                let result =  R.prop('letters', counted);
+
+                expect(14).toEqual(result);
             });
         });
     });
