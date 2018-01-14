bbff04b9cc1e50874100275057167fe7ae19a8a6
Tom <thoschulte@gmail.com>
2018-01-14 01:52:37 +0100

    update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp'),
     shell = require('shelljs');
 
+
+const tar = require('gulp-tar');
+const gzip = require('gulp-gzip');
+
 gulp.task('default', defaultTask);
 
 function defaultTask(done) {
@@ -18,14 +22,19 @@ function defaultTask(done) {
         'bin/letter-count.js'
     ]).pipe(gulp.dest('./build/bin'));
 
-    gulp.src('lib/app.js').pipe(gulp.dest('./build/lib'));
+    gulp.src('lib/app.js').pipe(gulp.dest('build/lib'));
 
     gulp.src([
         'test/input.txt',
         'test/testrunner.js'
     ]).pipe(gulp.dest('./build/test'));
 
-    gulp.src('spec/**/*').pipe(gulp.dest('./build/spec'));
+    gulp.src('spec/**/*').pipe(gulp.dest('build/spec'));
+
+    gulp.src('build/*')
+        .pipe(tar('letter-count.tgz'))
+        //.pipe(gzip())
+        .pipe(gulp.dest('build'));
 
     done();
 };
