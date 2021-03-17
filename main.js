2ad1a4a480bd60ccb0bd7f0c80e9fca57f9064e9
DESKTOP-NUTUO9P\thoma <thoschulte@googlemail.com>
2021-03-18 00:10:26 +0100

    misc

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
 const http = require('http'),
     url = require('url'),
+    readline = require('readline')
     R = require('ramda');
 
 const LC = require('./lib'); // const LC = require('./lib/index');
 
-http.createServer((req, res) => {
-    let count = null;
-    let option = null;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    req.on('readable',() => {
-        const queryObject = url.parse(req.url, true).query
+process.stdin.on('data',data => {
+    const payload = data.toString();
+    const reg = new RegExp('[8-9][0-9][0-9][0-9]');
+    const port = (payload === undefined || payload === null || !reg.test(payload)) ? '8080' : payload;
+    let counter = 0;
 
-        count = R.isNil(queryObject.count) ? '' : queryObject.count;
-        option = R.isNil(queryObject.option) ? '-a' : queryObject.option;
-    });
+    http.createServer((req, res) => {
+        let count = null;
+        let option = null;
 
-    req.on('end', () => {
-        res.writeHead(200, {
-            'content-type': 'application/json'
+
+
+        rl.question("Run: ", param => {
+            console.log(param);
         });
 
-        res.end(JSON.stringify(LC.count(option, count)));
-    });
+        req.on('readable',() => {
+            const queryObject = url.parse(req.url, true).query
 
-    req.on('close', () => {
-        console.log('closed');
-    });
+            count = R.isNil(queryObject.count) ? '' : queryObject.count;
+            option = R.isNil(queryObject.option) ? '-a' : queryObject.option;
+            counter++;
+        });
 
-}).listen(8080);
+        req.on('end', () => {
+            res.writeHead(200, {
+                'content-type': 'application/json'
+            });
+
+            res.end(JSON.stringify(LC.count(option, count)));
+        });
+
+        req.on('close', () => {
+            console.log(`${counter}`);
+            // process.stdout.write(`${counter}`);
+        });
+    }).listen(port);
+});
+
