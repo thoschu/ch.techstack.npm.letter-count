6a88643a23c6f298686b959df4d9bb536d651294
thoschu <thoschulte@googlemail.com>
2022-10-26 10:57:18 +0200

    no message

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,28 @@
+// declare module 'letter-count' {
+//   export function countFromFile(
+//     option: string,
+//     path: string
+//   ): CountResult;
+//
+//   export const count: (option: string, path: string) => CountResult;
+//   export const info: () => CountResult;
+//
+//   type CountResult = Record<string, string | number>;
+// }
+// index.d.ts
 declare module 'letter-count' {
-  function countFromFile(
-    option: string,
-    path: string
-  ): Record<string, string | number>;
+  interface CountParamI {
+    option?: string;
+    value: string;
+    path: string;
+  }
 
-  const count: (option: string, path: string) => Record<string, string | number>;
-  const info: () => Record<string, string | number>;
+  export type CountResult = Record<string, string | number>;
+  export type CountParam = Omit<CountParamI, 'path'> | string;
+  export type CountFromFileParam = Omit<CountParamI, 'value'> | string;
+
+  //export const count: (option: string, value: string) => CountResult;
+  export const count: (value: CountParam) => CountResult;
+  export function countFromFile(value: CountFromFileParam): CountResult;
+  export const info: () => CountResult;
 }
